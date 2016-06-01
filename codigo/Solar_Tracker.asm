3e6131c9a29da1801014bc7349b479d5c2d32c79
Mecanigio <mecanigio@hotmail.com>
2016-06-01 15:25:54 -0300

    pase las directivas de preprocesador al header general del proyecto

--- codigo/Solar_Tracker.asm
+++ codigo/Solar_Tracker.asm
@@ -1,15 +1,17 @@
-;*****************************************************
-;* 	Programa Principal Seudo Codigo
+;*************************************************************
+;*				Programa Principal Seudo Codigo
 ;*
-;*  Created: 29/05/2016
-;*  Autor: Joaquín Ulloa
+;*					Created: 29/05/2016
+;*
+;*  Autors: Joaquín Ulloa, Agustin Picard, Mauro Giordano
 ;*	
-;***************************************************** 
+;*************************************************************
 
 .include "m88def.inc"   	;Incluye los nombres de los registros del micro
 .include "avr_macros.inc"	;Incluye los macros
 .listmac					;Permite que se expandan las macros en el listado
 
+.include "Solar_Tracker.inc"
 .include "ADC.inc"
 .include "PWM.inc"
 .include "SERIAL_PORT.inc"
@@ -35,8 +37,9 @@ RJMP	ISR_REG_USART_VACIO
 
 ;Interrupciones
 
+;-------------------------------SETUP--------------------------------------------
 SETUP:
-;Se inicializa el stack pointer
+
 	LDI PWM_DATA,LOW(RAMEND)
 	OUT SPL,PWM_DATA
 	LDI	PWM_DATA,HIGH(RAMEND)
@@ -45,39 +48,69 @@ SETUP:
 	RCALL ADC_INIT ;TIENE QUE ESTAR EN "ADC.inc"
 	RCALL PWM_INIT ;TIENE QUE ESTAR EN "PWM.inc"
 	RCALL SP_INIT ;TIENE QUE ESTAR EN "SERIAL_PORT.inc"
+	RCALL LIGHT_TURN_OFF
 
+	CLT	;[T=1]: ESTA CONECTADO A BT. [T=0]: NO ESTA CONECTADO A BT.
 	SEI
-;Inicializacion
+;--------------------------------------------------------------------------------
 
+;-------------------------------PROGRAMA_PRINCIPAL-------------------------------
 MAIN:	
-	medir bateria:
-		leer adc bat
-		comparar
-		si menor min jump a bajo consumo
+	;MEDIR BATERIA 
+		RCALL	READ_BATTERY					;MIDO LA BATERIA
+		CPI		ADC_DATA_H,MIN_BATTERY_VALUE	;COMPARAR PARA VER SI HAY SUFICIENTE BATERIA PARA OPERAR
+		BRLO	LOW_BATTERY						;SI ES MENOR AL MINIMO, VA A BAJO CONSUMO
 ;Joaco: "Habria que pense si de todas formas no hay que mover el panel"
 ;Mauro:	"Para mi no. Si no tiene energía, que no gaste. "
-	dia o noche:
-		leerLDRs
-		comparar
-		luz:
-			verificar si hay que prender luz
-			verificar si la luz esta prendida
-			si cumple ambos prender
-;Mauro: "AL TENER UNA OPCION DE PRENDER LA LUZ CUANDO SE MANDA POR BT, CREO QUE SE CHEQUEA SI PRENDER O NO AHI; OSEA QUE NO IRÍA ACA ESO CREO."
-		si no hay sol jump a bajo consumo
-	track:
-		leerLDRs
-		comparar
-		si no hay diferencia salgo del loop
-		moverMotores
+
+;Mauro: "Creo que para ver si es de dia o noche leemos los 4 LDRs y nos quedamos con el minimo"
+
+;	¿DIA O NOCHE?
+		RCALL	READ_MINIMUM_LDR				;LEE LOS 4 LDRS Y DEJA EL MINIMO EN ...........
+		RCALL	COMPARE_IF_DAY_OR_NIGHT			;[CARRY=1]: ES DE DIA. [CARRY=0]: ES DE NOCHE.
+		BRCC	AT_NIGHT						;Me aseguro que esta funcione no modifique SREG 
+		BRCS	AT_DAY							;Me aseguro que esta funcione no modifique SREG
+CONTINUE:
+;SI ESTOY ACA YA TENGO BATERIA SUFICIENTE Y DECIDI SI PRENDER O NO LA LUZ Y ES DE DIA.
+		RCALL	ORIENTATE_SOLAR_PANEL			;HAY QUE RESOLVER ESTO TODAVIA.
+;	track:
+;			leerLDRs
+;			comparar
+;			si no hay diferencia salgo del loop
+;			moverMotores
+;			volver a track
+;		detenerMotores
+
 ;Joaco: "hay que pensar si harcodiamos cuanto tiempo se mueven los motores dependiendo la diferencia entre LDR o si hacemos otra cosa"
 ;Mauro: "como leemos de 0-5v y el adc es de 10bits [1023 valores = 4,8mV por muestra]; shifteamos 2 lugares el adc y perdemos 15mV y listo.
 ;		En síntesis: comparamos hasta que sea igual [control por lazo cerrado] y listo." 
-		detenerMotores
-		volver a track
-	leer panel
-	bajo consumo	
-RJMP MAIN
 
+;	leer panel
+		BRTS MAIN
+		RJMP SLEEP_MODE
+		
 
-;Nos falta saber como hacer una interrupcion serie y como ir a modo bajo consumo
+;------------------------------MAIN_FUNCTIONS-----------------------------------
+LOW_BATTERY:
+	RCALL LIGHT_TURN_OFF
+	RJMP SLEEP_MODE
+
+SLEEP_MODE:
+;HAY QUE HACER COSAS ANTES DE IR A SLEEP, COMO APAGAR EL ADC Y NO SE QUE MAS
+;HAY 2 FORMAS DE SALIR DE SLEEP: EN UN TIEMPO t [TIEMPO ENTRE MEDICION Y MEDICION] Y POR CONEXION BT.
+
+	SLEEP
+	RJMP MAIN
+
+AT_NIGHT:
+		RCALL LIGHT_TURN_ON
+		RJMP SLEEP_MODE
+
+AT_DAY:
+		RCALL LIGHT_TURN_OFF
+		RJMP CONTINUE
+
+;JOACO: "Nos falta saber como hacer una interrupcion serie y como ir a modo bajo consumo."
+;MAURO: "INTERRUPCION SERIE YA ESTA. IR A BAJO CONSUMO HAY QUE PREGUNTAR SI ESTA BIEN ASI."
+
+.include "MESSAGES.inc"
